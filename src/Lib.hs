b327f42f614f8a53d683aa8cb3b627d212f35bbe
Alexander Ben Nasrallah <me@abn.sh>
2019-06-18 20:26:35 +0200

    add Bookmark data type and parsing function

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,47 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE OverloadedStrings #-}
+
 module Lib
   ( someFunc
+  , writeBookmark
+  , readBookmark
+  , parseBookmark
+  , renderBookmark
+  , Bookmark(..)
   ) where
 
+import           Data.Text       as T
+import           Data.Text.IO    as T
+import           Protolude
+import           System.FilePath
+import           Text.Read
+
 someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+someFunc = T.putStrLn "someFunc"
+
+type Tag = Text
+
+data Bookmark =
+  Bookmark
+    { url         :: Text
+    , name        :: FilePath
+    , tags        :: [Tag]
+    , description :: Text
+    }
+  deriving (Show, Read, Eq)
+
+parseBookmark :: Text -> Bookmark
+parseBookmark = read . unpack
+
+renderBookmark :: Bookmark -> Text
+renderBookmark = pack . show
+
+writeBookmark :: FilePath -> Bookmark -> IO ()
+writeBookmark baseDir bm = do
+  let path = joinPath [baseDir, name bm]
+  writeFile path $ renderBookmark bm
+
+readBookmark :: FilePath -> FilePath -> IO Bookmark
+readBookmark baseDir bmName = do
+  let path = joinPath [baseDir, bmName]
+  parseBookmark <$> readFile path
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,37 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+import           Data.Text
+import           Lib
+import           Test.Hspec
+import           Test.QuickCheck
+
+newtype TText =
+  TText
+    { fromTText :: Text
+    }
+
+instance Arbitrary TText where
+  arbitrary = fmap (TText . pack) arbitrary
+
+arbitraryText :: Gen Text
+arbitraryText = fromTText <$> arbitrary
+
+newtype TBookmark =
+  TBookmark
+    { fromTBookmark :: Bookmark
+    }
+  deriving (Eq, Show)
+
+instance Arbitrary TBookmark where
+  arbitrary =
+    fmap TBookmark $
+    Bookmark <$> arbitraryText <*> arbitrary <*> listOf arbitraryText <*>
+    arbitraryText
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main =
+  hspec $
+  describe "Lib.parse/renderBookmark" $
+  it "parses rendered data the same" $
+  property $ \x ->
+    x === (TBookmark . parseBookmark . renderBookmark $ fromTBookmark x)
