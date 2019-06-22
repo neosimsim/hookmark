b057cd2c8ad00d050a23980e8d2a89bf4f861ac8
Alexander Ben Nasrallah <me@abn.sh>
2019-06-22 13:01:46 +0200

    add cli to add bookmarks

--- app/Main.hs
+++ app/Main.hs
@@ -6,10 +6,15 @@ module Main
   ) where
 
 import           Control.Applicative
+import           Data.Maybe
+import           Data.Text           as T
+import qualified Data.Text.IO        as T
 import           Data.Version        (showVersion)
 import           Distribution.Git
+import           Lib
 import           Options.Applicative
 import           Paths_hookmark
+import           System.Environment  (lookupEnv)
 import           Text.RawString.QQ
 
 programDescription :: String
@@ -18,22 +23,59 @@ programDescription = [r|Store, edit and search bookmarks.|]
 programVersion :: String
 programVersion = showVersion version ++ "-" ++ $(compileAbbrHash)
 
-data Command =
-  Version
+data Command
+  = Version
+  | AddBookmark
+      { addBaseDir     :: Maybe FilePath
+      , addTags        :: [Text]
+      , addDescription :: Bool
+      , addName        :: FilePath
+      , addUrl         :: Text
+      }
+  deriving (Show)
 
 optionParser :: Parser Command
 optionParser =
   Version <$
   switch (long "version" <> short 'v' <> help "Show version" <> hidden) <|>
-  subparser (command "version" (info (pure Version) (progDesc "Show version")))
+  hsubparser
+    (command "add" (info addParser (progDesc "Add bookmark")) <>
+     command "version" (info (pure Version) (progDesc "Show version")))
+
+addParser :: Parser Command
+addParser =
+  AddBookmark <$>
+  optional
+    (strOption
+       (short 'b' <>
+        help
+          "Base directory to store bookmarks in. Default $HOOKMARKHOME or $HOME/.hookmarkhome if unset")) <*>
+  many
+    (strOption
+       (long "tag" <> short 't' <>
+        help "Bookmark tag, can be used multiple times")) <*>
+  switch (short 'd' <> help "Prompt for description") <*>
+  strArgument
+    (metavar "name" <>
+     help "Name of the bookmark, can contain '/' to build hierarchies") <*>
+  strArgument (metavar "url" <> help "URL of the bookmark")
 
 main :: IO ()
 main = do
-  opts <-
+  opt <-
     execParser $
     info
       (optionParser <**> helper)
-      (header "hookmark — browser independent bookmarking" <>
-       progDesc programDescription)
-  case opts of
+      (fullDesc <> progDesc programDescription <>
+       header "hookmark — browser independent bookmarking")
+  case opt of
     Version -> putStrLn programVersion
+    AddBookmark dir t promptDesc n u -> do
+      d <-
+        if promptDesc
+          then T.getContents
+          else return mempty
+      baseFromEnv <- lookupEnv "HOOKMARKHOME"
+      writeBookmark
+        (fromMaybe "$HOME/.hookmark" $ dir <|> baseFromEnv)
+        (Bookmark u n t d)
